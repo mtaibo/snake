07dd8d4087da04bfa7453d803f1a3410f2f41109
Miguel Taibo <miguel.taibo@icloud.com>
2025-09-17 11:33:24 +0200

    Cambio de directorio

--- modules/submodules/Snake.py
+++ modules/submodules/Snake.py
@@ -5,34 +5,35 @@ class Snake():
 
         self.stdscr = stdscr
 
-        '''Relativizar las medidas del grid'''
-        # Grid dimensions
+        # Relative dimensions
         self.grid_height = 0
         self.grid_width = 0
 
         # Non customizable options
         self.lenght = 2 # Head not included
-        self.direction = ord('d')
+        self.direction = ord('s') # Initial direction
         self.location = {
-            'head' : (12, 24),
-            'body' : [
-                (12, 25), (12, 26),
-            ]
-        } 
+            'head' : (self.grid_height//2, self.grid_width//2),
+            'body' : [(self.grid_height//2-1, self.grid_width//2), 
+                      (self.grid_height//2-2, self.grid_width//2)]} 
 
         # Customizable options
         self.movement_keys = [ord('w'), ord('a'), ord('s'), ord('d')]
         self.snake_characters = {
-            'body': '█',
-            'head-up': '▲',
-            'head-down': '▼',
-            'head-left': '◀',
-            'head-right': '▶',
-        }
+            'head': {
+                ord('w'): '▲', # Head up
+                ord('a'): '◀', # Head left
+                ord('s'): '▼', # Head down
+                ord('d'): '▶', # Head right
+            }, 'body': '█'}
 
 
     def display(self):
-        pass
+
+        # Display head
+
+
+        for 
 
     def move(self, direction):
         pass
